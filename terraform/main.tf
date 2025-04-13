c44df6c02044b46a473844dae1192f3e2700b676
niraj.gupta <niraj.hojai@gmail.com>
2025-04-13 07:54:58 +0000

    adding OIDC

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,7 +30,7 @@ resource "aws_iam_role" "github_actions_role" {
         }
         Condition = {
           StringLike = {
-            "token.actions.githubusercontent.com:sub" : "repo:nirajku102/UC04-Lamda-deployment:ref:refs/heads/testing-OIDC"
+            "token.actions.githubusercontent.com:sub" : "repo:nirajku102/AWS-Fargate-Deployment-Track:ref:refs/heads/main"
           }
         }
       }
